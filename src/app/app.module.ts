fdd2f10fea891fe397aa6ef8b94d1cdb074f98bb
kacemjawahdou <kacem.jawahdou@esprit.tn>
2019-04-09 11:45:08 +0100

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CvComponent } from './cv/cv.component';
+import { DetailComponent } from './detail/detail.component';
+import { ItemComponent } from './item/item.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CvComponent,
+    DetailComponent,
+    ItemComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
